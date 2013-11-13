2b62e86a687d8778ae5baa2600129ff7de20a28e
Joao Henrique Gouveia <jhgouveia@gmail.com>
2013-11-13 15:44:03 -0200

    Create default.pp
    apt-get update
    apt-get dist-upgrade
    package: likewise-open

---
+++ default.pp
@@ -0,0 +1,43 @@
+exec { 'apt_get_update':
+        command => 'sudo apt-get update',
+        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
+}
+
+exec { 'apt_get_dist-upgrade':
+        command => 'sudo apt-get dist-upgrade',
+        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
+}
+
+package {'likewise-open':
+        ensure => present,
+        require => Exec['apt_get_update'], 
+}
+
+
+
+
+/*
+
+package {'apache2':
+        ensure => present,
+        require => Exec['apt_get_update'], 
+}
+
+package {'minidlna':
+        ensure => present,
+        require => Package['apache2']
+}
+
+exec { 'get_minidlna_conf':
+        command => 'wget http://www.gouveia.pro/dev/minidlna.conf -P /tmp/.',
+        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
+        require => Package['minidlna']
+}
+
+exec { 'copy_minidlna_conf':
+        command => 'sudo cp /tmp/minidlna.conf /etc/.',
+        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
+        require => Exec['get_minidlna_conf']
+}
+
+*/
