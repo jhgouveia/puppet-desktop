6f216acac9422982ca2930866794cde068e1d2e9
Joao Henrique Gouveia <jhgouveia@gmail.com>
2013-11-13 19:16:28 -0200

    Update default.pp
    Include many packages
    Comment DomainJoin-Cli Join (Must be executed only once)

--- default.pp
+++ default.pp
@@ -14,11 +14,88 @@ package {'likewise-open':
         require => Exec['apt_get_dist-upgrade'], 
 }
 
+package {'vim':
+        ensure => present,
+        require => Package['likewise-open'], 
+}
+
+package {'cifs-utils':
+	ensure => present,
+	require => Package['vim'],
+}
+
+package {'alacarte':
+	ensure => present,
+	require => Package['cifs-utils'],
+}
+
+package {'pidgin':
+	ensure => present,
+	require => Package['alacarte'],
+}
+
+package {'openjdk-7-jre':
+	ensure => present,
+	require => Package['pidgin'],
+}
+
+package {'icedtea-7-plugin':
+	ensure => present,
+	require => Package['openjdk-7-jre'],
+}
+
+package {'ubuntu-restricted-extras':
+	ensure => present,
+	require => Package['icedtea-7-plugin'],
+}
+
+package {'filezilla':
+	ensure => present,
+	require => Package['ubuntu-restricted-extras'],
+}
+
+package {'mysql-workbench':
+	ensure => present,
+	require => Package['filezilla'],
+}
+
+package {'keepass2':
+	ensure => present,
+	require => Package['mysql-workbench'],
+}
+
+package {'git':
+	ensure => present,
+	require => Package['keepass2'],
+}
+
+package {'flashplugin-installer':
+	ensure => present,
+	require => Package['git'],
+}
+
+
+/*
+
+# package example
+package {'':
+	ensure => present,
+	require => Package[''],
+}
+
+*/
+
+
+
+/*
+
+# Modify to ensure execution only if computer has not joined domain yet
 exec { 'likewise_join_domain':
         command => 'sudo domainjoin-cli join interlegis.gov.br gouveia',
         path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
         require => Package['likewise-open'],
 }
+*/
 
 
 
