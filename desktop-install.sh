fa418be43b885b59da7f6330ed4270fba29b0070
Joao Henrique Gouveia <jhgouveia@gmail.com>
2013-11-13 18:29:13 -0200

    Update desktop-install.sh
    Test sudo

--- desktop-install.sh
+++ desktop-install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Run system update
-sudo apt-get update
-sudo apt-get dist-upgrade
 
-# Install Puppet
-sudo apt-get install puppet
+# test for sudo
+if [ `whoami` != root ];
+then
+	#Run system update
+	sudo apt-get update
+	sudo apt-get dist-upgrade
 
-# Get and run Puppet script
-cd /tmp
-wget https://github.com/jhgouveia/puppet-desktop/blob/master/default.pp
-sudo puppet apply /tmp/default.pp
+	#Install Puppet
+	sudo apt-get install puppet
+
+	cd /tmp
+	wget https://github.com/jhgouveia/puppet-desktop/blob/master/default.pp
+	sudo puppet apply /tmp/default.pp
+
+else
+        echo "Por favor, execute este script utilizando SUDO";
+fi
