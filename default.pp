5ed279a592e504b0295086c2ca10c73291fc02d3
jhgouveia <jhgouveia@gmail.com>
2013-11-19 15:19:13 -0200

    Changed installation of ttf-mscorefonts-installer in default.pp.

--- default.pp
+++ default.pp
@@ -1,42 +1,25 @@
-exec { 'apt_update':
-        command => 'sudo apt-get update',
-        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
-}
-
-exec { 'apt_upgrade':
-        command => 'sudo apt-get -y dist-upgrade',
-        path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:',
-        require => Exec['apt_update'],
-}
-
 package {'likewise-open':
         ensure => present,
-        require => Exec['apt_upgrade'], 
 }
 
 package {'vim':
         ensure => present,
-        require => Package['likewise-open'], 
 }
 
 package {'cifs-utils':
 	ensure => present,
-	require => Package['vim'],
 }
 
 package {'alacarte':
 	ensure => present,
-	require => Package['cifs-utils'],
 }
 
 package {'pidgin':
 	ensure => present,
-	require => Package['alacarte'],
 }
 
 package {'openjdk-7-jre':
 	ensure => present,
-	require => Package['pidgin'],
 }
 
 package {'icedtea-7-plugin':
@@ -46,32 +29,26 @@ package {'icedtea-7-plugin':
 
 package {'ubuntu-restricted-extras':
 	ensure => present,
-	require => Package['icedtea-7-plugin'],
 }
 
 package {'filezilla':
 	ensure => present,
-	require => Package['ubuntu-restricted-extras'],
 }
 
 package {'mysql-workbench':
 	ensure => present,
-	require => Package['filezilla'],
 }
 
 package {'keepass2':
 	ensure => present,
-	require => Package['mysql-workbench'],
 }
 
 package {'git':
 	ensure => present,
-	require => Package['keepass2'],
 }
 
 package {'flashplugin-installer':
 	ensure => present,
-	require => Package['git'],
 }
 
 package { 'python-gpgme':
@@ -80,52 +57,52 @@ package { 'python-gpgme':
 
 package { 'vim-puppet':
 	ensure => present,
-	require => Exec['apt_update'],
 }
 
 package { 'libappindicator1':
 	ensure => present,
-	require => Package['vim-puppet'],
 }
 
 package { 'vlc':
         ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'xclip':
         ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'terminator':
         ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'playonlinux':
         ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'openssh-server':
         ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'pgadmin3':
 	ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
 package { 'linphone':
 	ensure => present,
-        require => Exec['apt_upgrade'],
 }
 
-package { 'ttf-mscorefonts-installer':
+class unifocus-context::msfonts {
+  exec { "accept-msttcorefonts-license":
+    command => "/bin/sh -c \"echo ttf-mscorefonts-installer msttcorefonts/accepted-     mscorefonts-eula select true | debconf-set-selections\""
+  }
+
+  package { "msttcorefonts":
+    ensure  => installed,
+    require => Exec['accept-msttcorefonts-license']
+  }
+}
+
+package { 'fonts-sil-gentium-basic':
 	ensure => present,
-        require => Exec['apt_upgrade'],
 }
-
 
